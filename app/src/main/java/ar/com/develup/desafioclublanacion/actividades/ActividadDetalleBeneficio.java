a423a05b65aeb77cc789e69a7f122cac56939cf8
maribel <maribel@develup.com.ar>
2015-04-11 05:15:18 -0300

    Mapa, cómo llegar y no volver a mostrar este beneficio

--- app/src/main/java/ar/com/develup/desafioclublanacion/actividades/ActividadDetalleBeneficio.java
+++ app/src/main/java/ar/com/develup/desafioclublanacion/actividades/ActividadDetalleBeneficio.java
@@ -1,14 +1,23 @@
 package ar.com.develup.desafioclublanacion.actividades;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -33,6 +42,7 @@ import ar.com.develup.desafioclublanacion.modelo.Tarjeta;
 import ar.com.develup.desafioclublanacion.modelo.Tarjetas;
 import ar.com.develup.desafioclublanacion.util.FechaUtil;
 import ar.com.develup.desafioclublanacion.util.FuentesUtil;
+import ar.com.develup.desafioclublanacion.util.Preferencias;
 import ar.com.develup.desafioclublanacion.util.SingletonRequestQueue;
 
 public class ActividadDetalleBeneficio extends ActividadBasica {
@@ -44,6 +54,55 @@ public class ActividadDetalleBeneficio extends ActividadBasica {
     private TextView descripcion;
     private TextView validoDesde;
     private TextView validoHasta;
+    private TextView noVolverAMostrar;
+    private TextView comoLlegar;
+    private SupportMapFragment mapa;
+    private OnMapReadyCallback mapaCargado = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+
+           ubicarBeneficio();
+        }
+    };
+    private Beneficio beneficio;
+    private View.OnClickListener noVolverAMostrarListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Preferencias.noMostrarBeneficio(ActividadDetalleBeneficio.this, beneficio.getId());
+
+            new MaterialDialog.Builder(ActividadDetalleBeneficio.this)
+                    .content(getString(R.string.no_veras_este_beneficio))
+                    .positiveText(R.string.aceptar)
+                    .negativeText(R.string.deshacer)
+                    .contentColorRes(android.R.color.background_dark)
+                    .positiveColorRes(android.R.color.background_dark)
+                    .negativeColorRes(android.R.color.darker_gray)
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            super.onPositive(dialog);
+                        }
+
+                        @Override
+                        public void onNegative(MaterialDialog dialog) {
+                            Preferencias.volverAMostrarBeneficio(ActividadDetalleBeneficio.this, beneficio.getId());
+                        }
+                    })
+                    .show();
+        }
+    };
+    private View.OnClickListener comoLlegarListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Double latitud = beneficio.getPunto().getLatitud();
+            Double longitud = beneficio.getPunto().getLongitud();
+
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                    Uri.parse("http://maps.google.com/maps?daddr=" + latitud + "," + longitud));
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected int getLayout() {
@@ -67,6 +126,12 @@ public class ActividadDetalleBeneficio extends ActividadBasica {
         this.validoDesde = (TextView) findViewById(R.id.valido_desde);
         this.validoHasta = (TextView) findViewById(R.id.valido_hasta);
         this.descripcion = (TextView) findViewById(R.id.descripcion_beneficio);
+        this.noVolverAMostrar = (TextView) findViewById(R.id.no_volver_a_mostrar);
+        this.comoLlegar = (TextView) findViewById(R.id.como_llegar);
+        this.mapa = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+        this.mapa.getMapAsync(this.mapaCargado);
+        this.noVolverAMostrar.setOnClickListener(this.noVolverAMostrarListener);
+        this.comoLlegar.setOnClickListener(this.comoLlegarListener);
 
         configurarFuentes();
     }
@@ -79,6 +144,8 @@ public class ActividadDetalleBeneficio extends ActividadBasica {
         FuentesUtil.aplicarFuente(FuentesUtil.Fuente.HELVETICA_LIGHT, descripcion, this);
         FuentesUtil.aplicarFuente(FuentesUtil.Fuente.HELVETICA_LIGHT_ITALIC, validoDesde, this);
         FuentesUtil.aplicarFuente(FuentesUtil.Fuente.HELVETICA_LIGHT_ITALIC, validoHasta, this);
+        FuentesUtil.aplicarFuente(FuentesUtil.Fuente.HELVETICA_MEDIUM, noVolverAMostrar, this);
+        FuentesUtil.aplicarFuente(FuentesUtil.Fuente.HELVETICA_MEDIUM, comoLlegar, this);
     }
 
     private void obtenerDetalleBeneficio() {
@@ -143,6 +210,10 @@ public class ActividadDetalleBeneficio extends ActividadBasica {
 
     private void mostrarDetalle(Beneficio beneficio) {
 
+        this.beneficio = beneficio;
+
+        this.ubicarBeneficio();
+
         getSupportActionBar().setTitle(beneficio.getEstablecimiento().getNombre());
         getAplicacion().mostrarImagen(beneficio.getUrlImagen(), foto);
         tipoBeneficio.setText(beneficio.getDetalle().getTipo());
@@ -158,7 +229,22 @@ public class ActividadDetalleBeneficio extends ActividadBasica {
 
         validoDesde.setText(FechaUtil.formatear(beneficio.getDesde()));
         validoHasta.setText(FechaUtil.formatear(beneficio.getHasta()));
-
     }
+
+    private void ubicarBeneficio() {
+
+        if (this.beneficio != null) {
+
+            LatLng beneficioLatLng = new LatLng(beneficio.getPunto().getLatitud(), beneficio.getPunto().getLongitud());
+            mapa.getMap().setMyLocationEnabled(true);
+            mapa.getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(beneficioLatLng, 16));
+
+            mapa.getMap().addMarker(new MarkerOptions()
+                    .title(this.beneficio.getDetalle().getNombre())
+                    .position(beneficioLatLng));
+        }
+    }
+
+
 
 }
